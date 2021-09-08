5ba595d59a67c0684990687b3a544846eab130cf
Joel July <joeljuly11@gmail.com>
2021-09-07 23:32:43 -0400

    Added Sections with Stylings and images

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -6,8 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    
+    <link rel="stylesheet" href="styles/master.css">
 
     <title>Landing Page</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding: 0;" >
+        <div class="container-fluid" id="my-nav">
+            
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" alt="LOGO" class="d-inline-block align-text-top" id="logo" ></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-white" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">ABOUT</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            SERVICES
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Virtual Reality</a></li>
+                            <li><a class="dropdown-item" href="#">Augmented Reality</a></li>
+                            <li><a class="dropdown-item" href="#">Folding Screen Technology</a></li>
+                            <li><a class="dropdown-item" href="#">Console Gaming Technology</a></li>
+                            <li><a class="dropdown-item" href="#">PC Gaming Technology</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="nav-items">
+                        <a class="nav-link text-white" href="#">CONTACT</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">SIGNUP</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- <div class="d-inline-block float-end">Test</div> -->
+        </div>
+    </nav>
+    
--- index.php
+++ index.php
@@ -1,9 +1,81 @@
 <?php 
     include_once "includes/header.inc.php";
 ?>
+<header>
+    <div class="container-fluid">
+        <div class="row" style="">
+            <div class="d-flex full-window align-item-center col-md-6 p-5" >
 
-<h1>Testing</h1>
-<p>This is currently using bootstrap</p>
+                <div class="text-center text-light my-auto" >
+                    <!-- <h3 class="text-light p-2">This is a new Beginning</h3> -->
+                    <h2 class="text-light p-2">A New World</h2>
+                    <h1 class="text-light p-2 mb-4 font-weight-bold">Within Sight</h1>
+                    <button type="button" class="btn btn-lg btn-outline-primary rounded-pill px-5" name="button"><span class="fs-6">Get Started</span></button>
+                </div>
+
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+
+	</div>
+
+</header>
+
+
+
+<div class="row" id="v_r" id="middle">
+
+    <div class="col-xs-12 col-sm-8 col-md-6" >
+
+        <img src="images\vr.png" alt="img1" class="img-fluid" id="img1">
+
+    </div>
+   
+
+    <div class="col-xs-12 col-sm-8 col-md-6" id="v_r">
+
+        <h3>Virtual Reality Experiences are what we do.</h3>
+        <h4>We specialise in AR and VR development because this 
+            industry provides cutting edge solutions and products that
+            Help your company flourish.
+        </h4>
+
+        <button type="button" class="btn btn-secondary btn-lg">See More</button>
+
+    </div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-12" id="t-head">
+    
+    <h2><b>Our Team</b></h2>
+
+</div>
+
+<div class="row" id="team">
+
+    <div class="col-xs-12 col-sm-6 col-md-4" id="gallery">
+        
+        <img src="images\emp1.jpg" alt="Cinque Terre" width="700" height="500">
+        <div class="desc">O'Brian Austin<br> CEO</div>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-4" id="gallery">
+        
+        <img src="images\emp3.jpg" alt="Cinque Terre" width="700" height="500">
+        <div class="desc">John July<br> Client Manager</div>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-4" id="gallery">
+        
+        <img src="images\emp4.jpg" alt="Cinque Terre" width="700" height="500">
+        <div class="desc">Carolyn Mc’Lean<br> UX Designer</div>
+
+    </div>
+
+</div>
+
 
 <?php 
     include_once "includes/footer.inc.php";
