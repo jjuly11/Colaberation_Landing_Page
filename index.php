1625a2186b80d060ee4693ea9029cc3afab165f5
Joel July <joeljuly11@gmail.com>
2021-09-15 08:01:52 -0400

    General Build Out

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                     <!-- <h3 class="text-light p-2">This is a new Beginning</h3> -->
                     <h2 class="text-light p-2 fs-4">A NEW WORLD</h2>
                     <h1 class="text-light p-2 mb-4 fs-1 font-weight-bold">WITHIN SIGHT</h1>
-                    <button type="button" class="btn btn-lg btn-outline-primary rounded-pill px-5 bg-white" name="button"><span class="fs-6">Get Started</span></button>
+                    <button type="button" class="btn btn-lg btn-outline-primary header-btn rounded-pill px-5 bg-white" name="button"><span class="fs-6">Get Started</span></button>
                 </div>
 
             </div>
@@ -75,6 +75,142 @@
     </div>
 
 </div>
+
+<section id="services">
+
+    <center><h2>Services</h2></center>
+
+        <div class="row1">
+
+            <div class="col-xs-12 col-sm-6 col-md-6" id="service-text">
+            <h1 id="heads">console gaming<br>Technolog</h1>
+
+                <b><ol>
+                <li>HTC Vive Headset</li>
+                <li>Computer</li>
+                <li>VR Controller</li>
+                <li>Cleaning Equipment</li>
+                </ol></b>
+            </div>
+
+                <img src="\images\service1.jpeg" class="img-fluid" alt="ser-img-1" width="500" height="400" id="ser-img1">
+            
+                <img src="\images\service2.png" class="img-fluid" alt="ser-img-1" width="500" height="400" id="ser-img2">
+
+            <div class="col-xs-12 col-sm-6 col-md-6" id="service-text2">
+            <h1 id="heads2">Virtual Reality Lands</h1>
+
+                <b><ol>
+                <li>Dreadhalls</li>
+                <li>Space x Hunter VR</li>
+                <li>Aquarium VR</li>                
+                <li>InCell</li>
+                </ol></b>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-6" id="service-text">
+            <h1 id="heads">console gaming<br>Technolog</h1>
+
+                <b><ol>
+                <li>HTC Vive Headset</li>
+                <li>Computer</li>
+                <li>VR Controller</li>
+                <li>Cleaning Equipment</li>
+                </ol></b>
+            </div>
+
+                <img src="\images\service1.jpeg" class="img-fluid" alt="ser-img-1" width="500" height="400" id="ser-img1">
+
+                <img src="\images\service4.png" class="img-fluid" alt="ser-img-1" width="500" height="400" id="ser-img2">
+
+            <div class="col-xs-12 col-sm-6 col-md-6" id="service-text2">
+            <h1 id="heads2">Virtual Reality Lands</h1>
+
+                <b><ol>
+                <li>Dreadhalls</li>
+                <li>Space x Hunter VR</li>
+                <li>Aquarium VR</li>                
+                <li>InCell</li>
+                </ol></b>
+            </div>
+        
+        </div>
+
+</section>
+
+<section id="subscription">
+    <div class="container-fluid d-flex text-center justify-content-between">
+        <div class="card p-5 overflow-hidden sub-card">
+            <div class="card-body">
+                <h2 class="card-title sub-title mb-5">PERSONAL</h2>
+                <h4 class="card-text mb-3">Per Month</h4>
+                <h3 class="card-text mb-4">$10.99</h3>
+                <div class="btn card-link sub-btn fs-3">SUBSCRIBE</div>
+            </div>
+        </div>
+
+        <div class="card p-5 overflow-hidden sub-card">
+            <div class="card-body">
+                <h2 class="card-title sub-title mb-5">SMALL TEAM</h2>
+                <h4 class="card-text mb-3">Per Month</h4>
+                <h3 class="card-text mb-4">$15.99</h3>
+                <div class="btn card-link sub-btn fs-3">SUBSCRIBE</div>
+            </div>
+        </div>
+        
+        <div class="card p-5 overflow-hidden sub-card">
+            <div class="card-body">
+                <h2 class="card-title sub-title mb-5">ENTERPRISE</h2>
+                <h4 class="card-text mb-3">Per Month</h4>
+                <h3 class="card-text mb-4">$20.99</h3>
+                <div class="btn card-link sub-btn fs-3">SUBSCRIBE</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="sign-up">
+    
+    <div class="s-upcontainter">
+    <h1>sign up</h1>
+
+   
+
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+ 
+ 
+  <div>
+  <input type="email" class="form-control" placeholder="Email"  aria-describedby="basic-addon1">
+  <input type="password" class="form-control" placeholder="Password"  aria-describedby="basic-addon1" id="pword" >
+  <p id="form-paragraph">Forgot your password?</p>
+  <button class="btn-class">SIGN IN</button>
+  <p id="createacc">Create account</p>
+  
+</div>
+
+
+
+<div>
+ 
+
+
+
+</div>
+
+</div>
+
+        
+</div>
+
+
+
+</section>
+
+
+
+
 
 
 <?php 
